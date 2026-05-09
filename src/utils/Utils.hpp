a7b06b6cc3be9953a7ac41af460a431f8ce302bb
Road Hog Studios <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-05-09 19:38:27 -0400

    Clean up unused function declarations in Utils.hpp
    Removed several unused function declarations from Utils.hpp.

--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -3,34 +3,6 @@ using namespace geode::prelude;
 
 namespace Odyssey
 {
-    //  Dialogos
-    DialogLayer *createDialog(const char *event, int background = 1, bool sameName = true);
-    gd::string createText(const char *english, const char *spanish = "");
-
-    //  Nodos para el Level Popup
-    CCNode *createDifficultyNode(GJDifficulty, int, bool);
-    CCNode *createProgressBar(int, bool);
-
-    //  Funciones
-    void addCorners(CCLayer *, const char *, float);
-    void insertAssetsToMap(bool, std::vector<int>);
-
-    void verifyVaultHints();
-    void hasAllVaultRewards();
-
-    void setSpanish(bool value);
-    bool isSpanish();
-
-    //  Progress Functions
-    int getMaxComic();
-    int getMainGameProgress();
-    int getFullGameProgress();
-
-    int getIslandForLevel(int levelID);
-
-    int getLevelSongID(int levelID);
-    std::pair<gd::string, gd::string> getLevelAudioAssets(int levelID);
-
     void patch(int, geode::ByteVector const &);
     std::vector<unsigned char> intToBytes(int);
 };
